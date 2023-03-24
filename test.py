38d8bbb566dc3f85a1366323e12ade212ceda343
이안이다 <71820380+iank1m@users.noreply.github.com>
2023-03-24 14:15:50 +0900

    Update test.py
    기존 29줄의 코드를 간략화하여 18줄로 줄여서 commit합니다.
    또한 f-string을 사용하여 변수를 출력하면 더 효율이 좋을 것 같습니다.
    
    확인 후 merge 바랍니다.

--- test.py
+++ test.py
@@ -1,28 +1,18 @@
-print("Hello, World!\n")
+# Hello, World! 출력
+print("Hello, World!")
 
-// 관련자의 정보를 삭제 후 형식에 맞춰 재작성
-int grade=2
-int my_gd=20
-int my_age=23
-string name_sc="한국 외국어 대학"
-char name="이상도"
-char ht="김해"
+# 내 정보 출력
+ht = "김해"
+name = "이상도"
+name_sc = "한국 외국어 대학"
+grade = 2
+my_gd = 20
+my_age = 23
 
+print(f"저는 {ht}에서 온 {name} 이라고 합니다.")
+print(f"저의 대학은 {name_sc}이고 저의 학년은 {grade}이며, {my_gd} 학점을 듣고 있습니다.")
+print(f"믿기지 않겠지만, 저의 나이는 {my_age} 살 입니다.")
 
-print("저는 %d에서 온 %c 이라고 합니다.\n" %(ht,name))
-
-print("저의 대학은 %s이고 저의 학년은 %d이며, %d 학점을 듣고 있습니다." %(name_sc,grade,my_gd))
-
-// 관련자의 가족 관계이력 추가
-엄마, 아빠, 누나, 나;
-
-=======
-//추가적인 내용이 필요.
-
-print("저의 대학은 %s이고 저의 학년은 %d이며, %d 학점을 듣고 있습니다.\n" %(name_sc,grade,my_gd))
-print("믿기지 않겠지만, 저의 나이는 %d 살 입니다.\n" %(age)
-
-for i in range(1,11) {
-  printf("%c", name) 
-}
-
+# 이름 10번 출력
+for i in range(10):
+    print(name, end='')
